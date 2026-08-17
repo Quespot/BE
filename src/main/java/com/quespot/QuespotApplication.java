8d1cd01f56a1085b576cba6adc6cc0489eacb6e8
dev.geonwoo_ <woo6629058@catholic.ac.kr>
2026-08-17 16:25:47 +0900

    chore: init quespot project

---
+++ src/main/java/com/quespot/QuespotApplication.java
@@ -0,0 +1,13 @@
+package com.quespot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuespotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuespotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quespot/QuespotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quespot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuespotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
