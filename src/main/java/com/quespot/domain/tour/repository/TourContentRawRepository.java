3c53149c1a5fb74803ce0ff30e4a84cc8f442815
Chhun-Lee <ckzlsn87@gmail.com>
2026-09-07 19:42:26 +0900

    [Feat]#28 정제 배치용 리포지토리 메서드 추가
    TourContentRawRepository에 operation별 api_modified_time 내림차순 조회를,
    SpotRepository(신규)에 source+source_content_id 단건 조회를 추가한다.

--- src/main/java/com/quespot/domain/tour/repository/TourContentRawRepository.java
+++ src/main/java/com/quespot/domain/tour/repository/TourContentRawRepository.java
@@ -3,5 +3,9 @@ package com.quespot.domain.tour.repository;
 import com.quespot.domain.tour.entity.TourContentRaw;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TourContentRawRepository extends JpaRepository<TourContentRaw, Long> {
+
+    List<TourContentRaw> findByOperationOrderByApiModifiedTimeDesc(String operation);
 }
